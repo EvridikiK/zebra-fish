da24b58de120096bfa4db68ad9b002bf302c51f3
Diogo Oliveira <diogo.oliveira898@gmail.com>
2026-02-23 15:27:24 +0000

    pars_init.m now stores parameters obtained from DEBInitNet

--- data_2p5/pars_init_Danio_rerio.m
+++ data_2p5/pars_init_Danio_rerio.m
@@ -7,22 +7,22 @@ par.T_ref = 293.15;   free.T_ref = 0;   units.T_ref = 'K';        label.T_ref = 
 
 %% core primary parameters
 par.T_A = 8000;       free.T_A   = 0;   units.T_A = 'K';          label.T_A = 'Arrhenius temp';
-par.z = 0.36847;      free.z     = 1;   units.z = '-';            label.z = 'zoom factor';
+par.z = 0.3236;       free.z     = 1;   units.z = '-';            label.z = 'zoom factor';
 par.F_m = 6.5;        free.F_m   = 0;   units.F_m = 'l/d.cm^2';   label.F_m = '{F_m}, max spec searching rate';
 par.kap_X = 0.8;      free.kap_X = 0;   units.kap_X = '-';        label.kap_X = 'digestion efficiency of food to reserve';
 par.kap_P = 0.1;      free.kap_P = 0;   units.kap_P = '-';        label.kap_P = 'faecation efficiency of food to faeces';
-par.v = 0.022486;     free.v     = 1;   units.v = 'cm/d';         label.v = 'energy conductance';
-par.kap = 0.77325;    free.kap   = 1;   units.kap = '-';          label.kap = 'allocation fraction to soma';
+par.v = 0.0189;       free.v     = 1;   units.v = 'cm/d';         label.v = 'energy conductance';
+par.kap = 0.4248;     free.kap   = 1;   units.kap = '-';          label.kap = 'allocation fraction to soma';
 par.kap_R = 0.95;     free.kap_R = 0;   units.kap_R = '-';        label.kap_R = 'reproduction efficiency';
-par.p_M = 118.03;     free.p_M   = 1;   units.p_M = 'J/d.cm^3';   label.p_M = '[p_M], vol-spec somatic maint';
+par.p_M = 468.5;      free.p_M   = 1;   units.p_M = 'J/d.cm^3';   label.p_M = '[p_M], vol-spec somatic maint';
 par.p_T = 0;          free.p_T   = 0;   units.p_T = 'J/d.cm^2';   label.p_T = '{p_T}, surf-spec somatic maint';
 par.k_J = 0.002;      free.k_J   = 0;   units.k_J = '1/d';        label.k_J = 'maturity maint rate coefficient';
-par.E_G = 5237.3781;  free.E_G   = 1;   units.E_G = 'J/cm^3';     label.E_G = '[E_G], spec cost for structure';
-par.E_Hb = 1.043e-01; free.E_Hb  = 1;   units.E_Hb = 'J';         label.E_Hb = 'maturity at birth';
-par.E_Hj = 3.255e-01; free.E_Hj  = 1;   units.E_Hj = 'J';         label.E_Hj = 'maturity at metam';
-par.E_Hp = 6.565e+01; free.E_Hp  = 1;   units.E_Hp = 'J';         label.E_Hp = 'maturity at puberty';
-par.h_a = 3.985e-09;  free.h_a   = 1;   units.h_a = '1/d^2';      label.h_a = 'Weibull aging acceleration';
-par.s_G = 0.0001;     free.s_G   = 0;   units.s_G = '-';          label.s_G = 'Gompertz stress coefficient';
+par.E_G = 5230;       free.E_G   = 1;   units.E_G = 'J/cm^3';     label.E_G = '[E_G], spec cost for structure';
+par.E_Hb = 8.765e-01; free.E_Hb  = 1;   units.E_Hb = 'J';         label.E_Hb = 'maturity at birth';
+par.E_Hj = 9.421e+00; free.E_Hj  = 1;   units.E_Hj = 'J';         label.E_Hj = 'maturity at metam';
+par.E_Hp = 2.291e+03; free.E_Hp  = 1;   units.E_Hp = 'J';         label.E_Hp = 'maturity at puberty';
+par.h_a = 1e-8;       free.h_a   = 1;   units.h_a = '1/d^2';      label.h_a = 'Weibull aging acceleration';
+par.s_G = 1e-4;       free.s_G   = 0;   units.s_G = '-';          label.s_G = 'Gompertz stress coefficient';
 
 %% other parameters
 par.V_0 = 1e-10;         free.V_0   = 0;   units.V_0 = 'cm^3';       label.V_0 = 'initial structure of egg';
